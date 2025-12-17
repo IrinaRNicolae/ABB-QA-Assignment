b243d766e4fe322a644d3df084c0325a133c6332
IrinaRNicolae <irina.nicolae@betterask.erni>
2025-12-17 19:51:53 +0100

    Update Main.java

--- library/books-tests/src/main/java/books/Main.java
+++ library/books-tests/src/main/java/books/Main.java
@@ -2,6 +2,6 @@ package books;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Books search");
     }
 }
